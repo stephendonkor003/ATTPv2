a86219f95f707240a3a534bbbe1b252659a9d13d
Stephen Amoakoh Donkor <73423425+stephendonkor003@users.noreply.github.com>
2026-05-23 18:22:14 +0000

    Updated project

--- resources/views/procurement/purchase-orders/index.blade.php
+++ resources/views/procurement/purchase-orders/index.blade.php
@@ -166,7 +166,7 @@
                                         </a>
                                         @can('finance.purchase_orders.delete')
                                             <form method="POST"
-                                                action="{{ route('procurement.purchase-orders.destroy', $purchaseOrder) }}"
+                                                action="{{ url('procurement/purchase-orders/' . $purchaseOrder->getKey()) }}"
                                                 onsubmit="return confirm('Delete this purchase order? Payment records will be kept but detached from this order.');">
                                                 @csrf
                                                 @method('DELETE')
